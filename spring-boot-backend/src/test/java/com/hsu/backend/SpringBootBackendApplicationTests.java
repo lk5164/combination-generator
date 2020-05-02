4b197226009ba719c25e64b1f67bbcf55d50aab1
Pengfei Lun <plun@seas.upenn.edu>
2020-05-02 17:57:46 -0400

    Updated Test Files

--- spring-boot-backend/src/test/java/com/hsu/backend/SpringBootBackendApplicationTests.java
+++ spring-boot-backend/src/test/java/com/hsu/backend/SpringBootBackendApplicationTests.java
@@ -3,61 +3,58 @@ package com.hsu.backend;
 import com.hsu.backend.model.Item;
 import com.hsu.backend.repository.ReferenceDataRepository;
 import com.hsu.backend.service.ReferenceDataService;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.Matchers.*;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(properties = "application-test.properties")
-@ActiveProfiles("test")
+@WebAppConfiguration
 class SpringBootBackendApplicationTests {
-
-	private static Set<Item> nine1One2 = new HashSet<>();
-	@Autowired
-	private ReferenceDataService referenceDataService;
 
 	@Autowired
 	private ReferenceDataRepository referenceDataRepository;
 
-	@Test
-	public void onlyRepositoryModuleIsLoaded() {
-		assertThat(referenceDataService).isNotNull();
-		assertThat(referenceDataRepository).isNotNull();
-	}
+	@Autowired
+	private WebApplicationContext webContext;
 
+	private MockMvc mockMvc;
 
-	@Test
-	public void whenPhoneNumberProvided_thenGenerateCombinationsIsCorrect() {
-		String ones = "1111111112";
-		nine1One2 = new HashSet<>();
-		nine1One2.add(new Item(1L, "1111111112"));
-		nine1One2.add(new Item(2L, "111111111a"));
-		nine1One2.add(new Item(3L, "111111111b"));
-		nine1One2.add(new Item(4L, "111111111c"));
-		Iterable<Item> mock = new Iterable<>() {
-			@Override
-			public Iterator<Item> iterator() {
-				return nine1One2.iterator();
-			}
-		};
-		Mockito.when(referenceDataService.generateCombinations(ones)).thenReturn(mock);
-		referenceDataService.generateCombinations(ones);
-		verify(this.referenceDataService).generateCombinations(ones);
+	@Before
+	public void setupMockMvc() {
+		this.mockMvc = MockMvcBuilders
+				.webAppContextSetup(webContext)
+				.build();
 	}
 
 	@Test
-	public void whenRequestingFirstPageOfSizeTwo_ThenReturnFirstPage() {
+	public void swaggerPage() throws  Exception {
+		this.mockMvc = MockMvcBuilders
+				.webAppContextSetup(webContext)
+				.build();
+		mockMvc.perform(MockMvcRequestBuilders.get("/swagger-ui.html"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void getItemsFirstPageOfSizeTwo() throws  Exception {
+		Set<Item> nine1One2 = new HashSet<>();
 		nine1One2 = new HashSet<>();
 		nine1One2.add(new Item(1L, "1111111112"));
 		nine1One2.add(new Item(2L, "111111111a"));
@@ -65,19 +62,39 @@ class SpringBootBackendApplicationTests {
 		nine1One2.add(new Item(4L, "111111111c"));
 		this.referenceDataRepository.saveAll(nine1One2);
 		this.referenceDataRepository.flush();
-		int s = 2, p = 0;
-		Iterable<Item> mock = new Iterable<>() {
-			@Override
-			public Iterator<Item> iterator() {
-				return nine1One2.iterator();
-			}
-		};
-		Set<Item> expected = new HashSet<>();
-		expected.add(new Item(1L, "1111111112"));
-		expected.add(new Item(2L, "111111111a"));
+		this.mockMvc = MockMvcBuilders
+				.webAppContextSetup(webContext)
+				.build();
+		mockMvc.perform(get("/reference/v1/getItems/size/2/page/1"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.content", hasSize(2)))
+				.andExpect(jsonPath("$.content[*].text", containsInAnyOrder(
+						"111111111b",
+						"111111111c"
+				)));
+	}
 
-		Mockito.when(referenceDataService.findByPage(s, p)).thenReturn(expected);
-		verify(this.referenceDataService).findByPage(s, p);
+	@Test
+	public void generateItems() throws  Exception {
+		this.mockMvc = MockMvcBuilders
+				.webAppContextSetup(webContext)
+				.build();
+		mockMvc.perform(post("/reference/v1/generate")
+				.content("1111111112"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(4)))
+				.andExpect(jsonPath("$[*].text", containsInAnyOrder(
+						"1111111112",
+						"111111111a",
+						"111111111b",
+						"111111111c"
+				)));
+
+	}
+
+	@Test
+	public void onlyRepositoryModuleIsLoaded() {
+		assertThat(referenceDataRepository).isNotNull();
 	}
 
 	@Test
